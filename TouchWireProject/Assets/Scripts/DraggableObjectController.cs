e11bd1daf5c545e284834e3f44367f476394eb5d
JoshuaStrunk <joshua.p.strunk@gmail.com>
2015-01-07 12:06:31 -0600

    draggable objects, powerable objects, basics of both

---
+++ TouchWireProject/Assets/Scripts/DraggableObjectController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DraggableObjectController : MonoBehaviour {
+
+
+
+    bool dragging;
+    Vector2 oldMouse;
+
+
+	// Use this for initialization
+	void Start () {
+        dragging = false;
+        oldMouse = Input.mousePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (dragging) {
+            Vector2 currMouse = getMousePosition();
+            Vector2 deltaMouse = -(oldMouse - currMouse);
+            transform.position += new Vector3(deltaMouse.x, deltaMouse.y, transform.position.z);
+            oldMouse = getMousePosition();
+        }
+	}
+
+    Vector2 getMousePosition() {
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+
+    void OnMouseDown() {
+        dragging = true;
+        oldMouse = getMousePosition();
+    }
+
+    void OnMouseUp() {
+        dragging = false;
+    }
+}
--- TouchWireProject/Assets/Scripts/PoweredObjectController.cs
+++ TouchWireProject/Assets/Scripts/PoweredObjectController.cs
@@ -5,14 +5,23 @@ public class PoweredObjectController : MonoBehaviour {
 
     public bool powered = false;
 
+    public Sprite poweredSprite;
+    public Sprite unPoweredSprite;
+
+    SpriteRenderer spriteRender;
+
 	// Use this for initialization
 	void Start () {
-	
+        spriteRender = GetComponent<SpriteRenderer>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (powered) {
+            spriteRender.sprite = poweredSprite;
+        }
+        else {
+            spriteRender.sprite = unPoweredSprite;
+        }
 	}
-
 }
